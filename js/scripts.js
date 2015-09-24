0eee0c0eb2dc40fc635d75f8edd9b5089180b91f
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-23 22:13:03 -0700

    Passes spec for switching turns from X to O

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,18 @@ Player.prototype.startMessage = function() {
   return this.mark + " starts the game";
 }
 
+Player.prototype.switchTurn = function() {
+  if (this.mark === "X") {
+    this.mark = "O";
+  } else {
+    this.mark = "X";
+  }
+}
+
 var setMark = function() {
   if (Math.random() < 0.5) {
     return "X";
   } else {
-    return "Y";
+    return "O";
   }
 }
-
-// var getTurn = function() {
-//
-// }
--- spec/specs.js
+++ spec/specs.js
@@ -4,22 +4,20 @@ describe('Player', function() {
       expect(testPlayer.mark).to.equal("X");
    });
 
-   it("returns a starting message if it is 'X' or 'Y' to start with", function() {
+   it("returns a starting message if it is 'X' or 'O' to start with", function() {
      var testPlayer = new Player("X");
      expect(testPlayer.startMessage()).to.equal("X starts the game");
+   });
+
+   it("switches turns for the player", function() {
+     var testPlayer = new Player("X");
+     testPlayer.switchTurn();
+     expect(testPlayer.mark).to.equal("O");
    });
 });
 
 describe('setMark', function() {
-  it("returns a randomly selected 'X' or 'Y' mark", function() {
+  it("returns a randomly selected 'X' or 'O' mark", function() {
     expect(setMark()).to.be.a('string');
   });
 });
-
-// describe('getTurn', function() {
-//   it("switches the player's turn", function() {
-//   var testPlayer = new Player("X");
-//   var mark =
-//   expect(getTurn()).to.equal("Y");
-//   });
-// });
